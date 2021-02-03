cf3e96e620231d66bb97d2c32cb6b33553dc57d0
prince <princekumarkeshri.22@gmail.com>
2021-02-03 19:41:50 +0530

    trial is modified

--- Trial.java
+++ Trial.java
@@ -1,7 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collection;
 class TrialClass{
     public static void main(String[] args) {
-        System.out.println("Hello git");
-        System.out.println("new");
+        // System.out.println("Hello git");
+        // System.out.println("new");.
+
+        ArrayList<String> i= new ArrayList<>();
+        
+        i.add("item1");
+        i.add("item2");
+        i.add("item3");
+        
+        String a [] = {"item1", "item2", "item3"};
+        
+        for (String item : i) {
+            System.out.print(item + " ");
+        }
+
+        for (String item1 : a) {
+            System.out.print(item1 + " ");
+        }
+
+        int integerArray[] = {5,2,2,0};
+        System.out.println();
+        int index =0 ;
+        for (int integer : integerArray) {
+            System.out.println(integer +  " " + index++);
+           
+        }
       
     }
 }
